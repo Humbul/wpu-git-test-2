482ee81f210542908fb7d7e850ca0ced1a7123b3
Fuad Izulhaq <fuadizulhaq93@gmail.com>
2025-02-16 01:26:21 +0700

    menambahkan file html dan css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum odio omnis veritatis enim harum? Sit iure inventore eos eum sint distinctio explicabo dolore in cupiditate vero, repudiandae alias corrupti id?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+</body>
+
+</html>
